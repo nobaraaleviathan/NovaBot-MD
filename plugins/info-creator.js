4f92bf0ec8f5d273aaa472a840126b5e638f26bc
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-06 21:16:00 -0300

    Delete plugins directory

--- plugins/info-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-handler.command = /^(owner|creator)$/i
-handler.register = true
-
-module.exports = handler
