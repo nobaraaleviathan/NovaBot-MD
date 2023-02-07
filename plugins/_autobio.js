6493413a7cf242db55618cd49bd3b4dbe7428788
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-07 15:38:30 -0300

    Add files via upload

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `🔰 Tiempo Activo: ${muptime}\n NovaBot-MD`
+		let bio = `ɴᴏᴠᴀʙᴏᴛ-ᴍᴅ | ᴀᴄᴛɪᴠᴏ ✅️: ${muptime}\nᴘᴀʀᴀ ᴠᴇᴢ ᴍɪ ʟɪsᴛᴀ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏ ᴜsᴇʀ #menu`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
