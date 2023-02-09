dca921f11457678aac2ce28f3cea457b4a85c788
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-09 09:21:38 -0300

    Add files via upload

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -6,7 +6,7 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn, usedPrefix, command}) => {
 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+if (!(who in global.db.data.users)) throw `ᴇʟ ᴜsᴜᴀʀɪᴏ ɴᴏ sᴇ ᴇɴᴄᴜᴇɴᴛʀᴀ ᴇɴ ᴍɪ ʙᴀsᴇ ᴅᴇ ᴅᴀᴛᴏs`
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
 let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
@@ -16,21 +16,27 @@ let math = max - xp
 let prem = global.prems.includes(who.split`@`[0])
 let sn = createHash('md5').update(who).digest('hex')
 
-let str = `
-┌───「 *PERFIL* 」
-▢ *🔖 Nombres:* 
-   • ${username} ${registered ? '\n   • ' + name + ' ': ''}
-   • @${who.replace(/@.+/, '')}
-▢ *📱Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-▢ *🔗Link:* wa.me/${who.split`@`[0]}${registered ? '\n▢ *🎈Edad*: ' + age + ' años' : ''}
-▢ *⚠️Advertencias:* ${warn}/${maxwarn}
-▢ *💎 Diamantes :* ${diamond}
-▢ *🆙 Nivel* : ${level}
-▢ *⬆️ XP* : Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `listo para *${usedPrefix}levelup*` : `_*${math}xp*_ Falta para subir de nivel`}
-▢ *🏆Rango:* ${role}
-▢ *📇 Registrado :* ${registered ? 'Si': 'No'}
-▢ *⭐ Premium* : ${prem ? 'Si' : 'No'}
-└──────────────`
+let str = `┏─━─━─━∞◆∞━─━─━─┓
+│🔸 ️🔖 ɴᴏᴍʙʀᴇ: ${name} ${user.registered === true ? 'ͧͧͧͦꙶͣͤ✓' : ''}
+│——————«•»——————
+│🔸️ 📱ɴᴜᴍᴇʀᴏ: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+│——————«•»——————
+│🔸 ️🔗ʟɪɴᴋ: wa.me/${who.split`@`[0]}${registered ? '\n│——————«•»——————\n│🔸 ️ᴇᴅᴀᴅ: ' + age + ' años' : ''}
+│——————«•»——————
+│🔸️ ⚠️ᴀᴅᴠᴇʀᴛᴇɴᴄɪᴀs: ${warn}/${maxwarn}
+│——————«•»——————
+│🔸 ️💎 ᴅɪᴀᴍᴀɴᴛᴇs : ${diamond}
+│——————«•»——————
+│🔸 ️🆙 ɴɪᴠᴇʟ : ${level}
+│——————«•»——————
+│🔸 ️⬆️ xᴘ : ${exp}
+│——————«•»——————
+│🔸 ️🏆ʀᴀɴɢᴏ: ${role}
+│——————«•»——————
+│🔸 ️📇 ʀᴇɢɪsᴛʀᴀᴅᴏs : ${registered ? 'Si': 'No'}
+│——————«•»——————
+│🔸 ️⭐ ᴘʀᴇᴍɪᴜᴍ : ${prem ? 'Si' : 'No'}
+┗─━─━─━∞◆∞━─━─━─┛`
     conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
     m.react(done)
 
